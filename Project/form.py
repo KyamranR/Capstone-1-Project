a1bd4bda9282f1d9e10031a57e2b04278431a30e
Kyamran Rizayev <kyamranr@gmail.com>
2024-06-27 19:32:28 -0400

    added form.py and created login form and register form

---
+++ Project/form.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, TextAreaField
+from wtforms.validators import InputRequired, Length
+
+
+class LoginForm(FlaskForm):
+    """User login form"""
+
+    email = StringField('Email:', validators=[InputRequired()])
+    password = PasswordField('Password:', validators=[InputRequired()])
+
+
+
+class RegistrationFrom(FlaskForm):
+    """User registration form"""
+
+    name = StringField('Name:', validators=[InputRequired(), Length(min=1, max=20)])
+    password = PasswordField('Password:', validators=[InputRequired(), Length(min=6, max=50)])
+    email = StringField('Email:', validators=[InputRequired(), Length(max=50)])
+    profile_pic = StringField('(Optional) Profile Photo')
