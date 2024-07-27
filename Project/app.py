d62913802aef01ef2fd88b6a8fd155b5240d7ab7
Kyamran Rizayev <kyamranr@gmail.com>
2024-07-27 19:37:25 -0400

    went back how it was before

--- Project/app.py
+++ Project/app.py
@@ -6,7 +6,8 @@ from form import LoginForm, RegistrationForm, EditUserProfileForm, EditCarInfoFo
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ.get('DATABASE_URL', 'postgresql:///car_lookup'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
